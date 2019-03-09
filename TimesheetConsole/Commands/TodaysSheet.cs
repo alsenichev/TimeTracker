72888b55955f668eaf3519c5e11fe5fb96ac0998
Aleksey Senichev <siggizmund@hotmail.com>
2019-03-09 09:54:49 +0300

    Overtime header fixed

--- TimesheetConsole/Commands/TodaysSheet.cs
+++ TimesheetConsole/Commands/TodaysSheet.cs
@@ -48,21 +48,25 @@ namespace TimesheetConsole.Commands
 
     private static string MoreToWork(Status status)
     {
+      return $"You've started today at {status.StartedAt:t}{PausePostfix(status)}" +
+             $".{Environment.NewLine}Your working day ends at {status.EndOfDay:t} in {FormatTimeSpan(status.Left)}.";
+    }
+
+    private static string PausePostfix(Status status)
+    {
       string pausePostfix =
         status.Pause == TimeSpan.Zero
           ? string.Empty
           : status.Pause > TimeSpan.Zero
             ? $" and took a {FormatTimeSpan(status.Pause)} pause"
             : $" and have {FormatTimeSpan(-status.Pause)} deposed";
-      return $@"You've started today at {status.StartedAt:t}{pausePostfix}.{Environment.NewLine}Your working day ends at {status.EndOfDay:t} in {FormatTimeSpan(status.Left)}.";
+      return pausePostfix;
     }
 
     private static string Overtime(Status status)
     {
-      string pausePostfix =
-        status.Pause.Equals(TimeSpan.Zero) ? string.Empty : $" and took a {FormatTimeSpan(status.Pause)} pause";
-      return
-        $"You've started today at {status.StartedAt:t}{pausePostfix}.{Environment.NewLine}It makes a {FormatTimeSpan(status.Overtime)} overtime.";
+      return $"You've started today at {status.StartedAt:t}{PausePostfix(status)}" +
+             $".{Environment.NewLine}It makes a {FormatTimeSpan(status.Overtime)} overtime.";
     }
 
     private Result<string> Execute(bool displayHeader)
